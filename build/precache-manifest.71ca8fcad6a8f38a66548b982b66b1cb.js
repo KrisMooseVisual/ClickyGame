ec439430e1793c4992698f277ad25b688df61f33
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-12-20 14:47:47 -0500

    github deploy changes

---
+++ build/precache-manifest.71ca8fcad6a8f38a66548b982b66b1cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafd9e84d1b4cbe62aeb7ae5c18c75f5",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "500e0bc005b56b7c7e0f",
+    "url": "/ClickyGame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ea216f4bceb70d720650",
+    "url": "/ClickyGame/static/css/main.319b8749.chunk.css"
+  },
+  {
+    "revision": "500e0bc005b56b7c7e0f",
+    "url": "/ClickyGame/static/js/2.2b6caa0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.2b6caa0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea216f4bceb70d720650",
+    "url": "/ClickyGame/static/js/main.382cc815.chunk.js"
+  },
+  {
+    "revision": "bbc142d714fdb9766ebb",
+    "url": "/ClickyGame/static/js/runtime-main.fad8b6de.js"
+  }
+]);
